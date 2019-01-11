5b26b0047af12211e6c94f771cc32fb32b6f5a60
Amir Dolkhani <amirdolkhani@gmail.com>
2019-01-11 13:56:00 +0100

    Add rarity function for all types of project

--- Treasure.py
+++ Treasure.py
@@ -5,6 +5,7 @@ import random
 # If there is monster in the room the treasure would picked up unless the player get defeated by monsters or escaped from the room.
 # Treasures are randomized in each room based on its usuality as shown in below chart.
 
+#   #   #   #   #   #   #   #
 # Treasure          	Value(Point)	Rarity
 # Loose coins	          2	             40%
 # Money pouch	          6	             20%
@@ -28,5 +29,38 @@ gold_jewelery = Treasure("gold_jewelery", 10, 15)
 gemstone = Treasure("gemstone", 14, 10)
 small_treasure_chest = Treasure("small_treasure_chest", 20, 5)
 
+def loose_coins_rarity():
+     roll = random.randint(1,100)
+if (loose_coins_rarity.roll >= 1 and loose_coins_rarity.roll<= 40):
+    put in map
+else:
+    dont put in map
 
 
+def money_pouch_rarity():
+     roll = random.randint(1,100)
+if (money_pouch_rarity.roll >= 1 and money_pouch_rarity.roll<= 20):
+    put in map
+else:
+    dont put in map
+
+    def gold_jewelery_rarity():
+     roll = random.randint(1,100)
+if (gold_jewelery_rarity.roll >= 1 and gold_jewelery_rarity.roll<= 15):
+    put in map
+else:
+    dont put in map
+
+    def gemstone_rarity():
+     roll = random.randint(1,100)
+if (gemstone_rarity.roll >= 1 and gemstone_rarity.roll<= 10):
+    put in map
+else:
+    dont put in map
+
+    def small_treasure_chest_rarity():
+     roll = random.randint(1,100)
+if (small_treasure_chest_rarity.roll >= 1 and small_treasure_chest_rarity.roll<= 5):
+    put in map
+else:
+    dont put in map
