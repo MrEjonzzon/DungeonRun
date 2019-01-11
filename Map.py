d560bf4761b2713b5b14d8c6eb62a0a7e65e57ad
Blu3foo <emil.jacobsson92@gmail.com>
2019-01-11 13:22:50 +0100

    Map creation done/Emil

--- Map.py
+++ Map.py
@@ -1,11 +1,15 @@
 mapcord = []
+currentcord = 0
 
+ne =
+
+
+# Mapsize creator
 def mapsize(x, y):
     for r in range(x):
         for k in range(y):
             mapcord.append([r, k])
     print(mapcord)
-
 
 diffpick = input('Choose difficulty!\n[1] Easy\n[2] Medium\n[3] Hard\n')
 if diffpick == '1':
@@ -15,7 +19,15 @@ elif diffpick == '2':
 elif diffpick == '3':
     mapsize(8, 8)
 
-startroom = input('Were would you like to start?\n[1] Northwest\n[2] Northeast\n[3] Southwest\n[4] Southeast')
 
-direction = input("Choose direction\n[W] to go north\n[A] to go west\n[S]To go south\n[D] to go east")
-if
+
+
+# Start room selection
+startroom = input('Were would you like to start?\n[1] Northwest\n[2] Northeast\n[3] Southwest\n[4] Southeast')
+if startroom == '1':
+    currentcord = 0, 0
+elif startroom == '2':
+    currentcord = 0, ne
+
+direction = input("Choose direction\n[W] to go north\n[A] to go west\n[S]To go south\n[D] to go east").lower()
+if direction == 'w':
