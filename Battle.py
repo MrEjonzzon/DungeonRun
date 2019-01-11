9540a0d255c423cde14fa0e2687fb2b1e2a8663c
albinekman <42805249+albinekman@users.noreply.github.com>
2019-01-11 14:41:05 +0100

    small changes in battle

--- Battle.py
+++ Battle.py
@@ -1,61 +1,38 @@
 import random
-
 import Game
 
-
 def roll_dice(roll):
-        result=int(0)
-
+        result = int(0)
         i = 0
         while i < roll:
             dice = random.randint(1, 6)
-
-            i=i+1
-            result=result+dice
+            i = i+1
+            result = result+dice
         return result
 
-
-
-
 def damage(loser):
-    result= 0
-
-    i = 0
     if loser == "monster":
-        Game.monster.health=Game.monster.health-1
-
+        Game.monster.health = Game.monster.health-1
     elif loser == "Hero":
-        Game.hero.health=Game.hero.health-1
-
+        Game.hero.health = Game.hero.health-1
 
 def attack_hero():
-
     hero_attack = roll_dice(Game.hero.attack)
-    monster_agility= roll_dice(Game.monster.agility)
-
+    monster_agility = roll_dice(Game.monster.agility)
     if hero_attack > monster_agility:
-       damage("monster")
-
+        damage("monster")
     elif monster_agility > hero_attack:
         print("haha stupid human")
 
-
 def attack_monster():
-
     monster_attack = roll_dice(Game.monster.attack)
     hero_agility = roll_dice(Game.hero.agility)
-
     if monster_attack > hero_agility:
         damage("Hero")
-
     elif hero_agility > monster_attack:
         print("haha u missed")
 
-
-
-
 def escape():
-
     chance = Game.hero.agility * 10
     flee = random.randint(1, 100)
     if flee < chance:
