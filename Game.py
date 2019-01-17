950ba002693b974d0e24d100b5f122543dd4ad68
Emil <mrejonzzon@gmail.com>
2019-01-17 16:20:28 +0100

    Added AI class with functions. Remade code in MAP class into functions so that they would not run when importing

--- Game.py
+++ Game.py
@@ -1,69 +1,84 @@
 import Creature
 import Battle
+import Ai
 
-choice = int(input("Choose a hero \n 1: Knight \n 2: Mage \n 3: Thief \n"))
-if choice == 1:
-    Hero = Creature.Knight
-    print("You are a knight\nAttack: " + str(Hero.attack) + "\nEndurance: " + str(Hero.endurance) + "\nAgility: " + str(
-        Hero.agility) + "\nInitiative: " + str(Hero.initiative) + "\nSpecial ability: You block the first hit from a monster" + "\n\n")
-elif choice == 2:
-    Hero = Creature.Mage
-    print("You are a mage\nAttack: " + str(Hero.attack) + "\nEndurance: " + str(Hero.endurance) + "\nAgility: " + str(
-        Hero.agility) + "\nInitiative: " + str(Hero.initiative) + "\nSpecial ability:  You have 80% chanse to escape" + "\n\n")
-elif choice == 3:
-    Hero = Creature.Theif
-    print("You are a theif\nAttack: " + str(Hero.attack) + "\nEndurance: " + str(Hero.endurance) + "\nAgility: " + str(
-        Hero.agility) + "\nInitiative: " + str(Hero.initiative) + "\nSpecial ability: You have 25% chanse to dubble your damage" +"\n\n")
-else:
-    print("Invalid Choice")
+def player_game():
+    choice = int(input("Choose a hero \n 1: Knight \n 2: Mage \n 3: Thief \n"))
+    if choice == 1:
+        Hero = Creature.Knight
+        print("You are a knight\nAttack: " + str(Hero.attack) + "\nEndurance: " + str(Hero.endurance) + "\nAgility: " + str(
+            Hero.agility) + "\nInitiative: " + str(Hero.initiative) + "\nSpecial ability: You block the first hit from a monster" + "\n\n")
+    elif choice == 2:
+        Hero = Creature.Mage
+        print("You are a mage\nAttack: " + str(Hero.attack) + "\nEndurance: " + str(Hero.endurance) + "\nAgility: " + str(
+            Hero.agility) + "\nInitiative: " + str(Hero.initiative) + "\nSpecial ability:  You have 80% chanse to escape" + "\n\n")
+    elif choice == 3:
+        Hero = Creature.Theif
+        print("You are a theif\nAttack: " + str(Hero.attack) + "\nEndurance: " + str(Hero.endurance) + "\nAgility: " + str(
+            Hero.agility) + "\nInitiative: " + str(Hero.initiative) + "\nSpecial ability: You have 25% chanse to dubble your damage" +"\n\n")
+    else:
+        print("Invalid Choice")
 
 
-def fight(monster_1, monster_2, monster_3, monster_4):
-    knight_count = 0
-    order = Battle.fight_order(Hero.initiative, monster_1.initiative)
-    current_health_hero = Hero.endurance
-    current_health_monster_1 = monster_1.endurance
-    current_health_monster_2 = monster_2.endurance
-    current_health_monster_3 = monster_3.endurance
-    current_health_monster_4 = monster_4.endurance
-    hero_order = Battle.roll_dice(Hero.initiative)
-    print(hero_order)
-    monster_1_order = Battle.roll_dice(monster_1.initiative)
-    print(monster_1_order)
-    monster_2_order = Battle.roll_dice(monster_2.initiative)
-    print(monster_2_order)
-    monster_3_order = Battle.roll_dice(monster_3.initiative)
-    print(monster_3_order)
-    monster_4_order = Battle.roll_dice(monster_4.initiative)
-    print(monster_4_order)
+    def fight(monster_1, monster_2, monster_3, monster_4):
+        knight_count = 0
+        order = Battle.fight_order(Hero.initiative, monster_1.initiative)
+        current_health_hero = Hero.endurance
+        current_health_monster_1 = monster_1.endurance
+        current_health_monster_2 = monster_2.endurance
+        current_health_monster_3 = monster_3.endurance
+        current_health_monster_4 = monster_4.endurance
+        hero_order = Battle.roll_dice(Hero.initiative)
+        print(hero_order)
+        monster_1_order = Battle.roll_dice(monster_1.initiative)
+        print(monster_1_order)
+        monster_2_order = Battle.roll_dice(monster_2.initiative)
+        print(monster_2_order)
+        monster_3_order = Battle.roll_dice(monster_3.initiative)
+        print(monster_3_order)
+        monster_4_order = Battle.roll_dice(monster_4.initiative)
+        print(monster_4_order)
 
-    '''while True:
-        print("hero hp:", current_health_hero)
-        print("monster hp:", current_health_monster)
-        if current_health_hero > 0 and current_health_monster > 0:
-            if order == "hero":
-                battle_choice = int(input("1: Attack\n2: Escape\n"))
-                if battle_choice == 1:
-                    current_health_monster = Battle.attack_hero(choice, Hero.attack, monster_1.agility, current_health_monster)
-                    order = "monster"
-                elif battle_choice == 2:
-                    if Battle.escape(choice, Hero.agility):
-                        break
-                    else:
+        '''while True:
+            print("hero hp:", current_health_hero)
+            print("monster hp:", current_health_monster)
+            if current_health_hero > 0 and current_health_monster > 0:
+                if order == "hero":
+                    battle_choice = int(input("1: Attack\n2: Escape\n"))
+                    if battle_choice == 1:
+                        current_health_monster = Battle.attack_hero(choice, Hero.attack, monster_1.agility, current_health_monster)
                         order = "monster"
-            elif order == "monster":
-                current_health_hero = Battle.attack_monster(monster_1.attack, Hero.agility, current_health_hero, choice, knight_count)
-                order = "hero"
-                knight_count = knight_count + 1
-            else:
-                print("Invalid Choice")
-        elif current_health_monster <= 0:
-            print("monster died")
-            break
-        elif current_health_hero <= 0:
-            print("you died")
-            break'''
+                    elif battle_choice == 2:
+                        if Battle.escape(choice, Hero.agility):
+                            break
+                        else:
+                            order = "monster"
+                elif order == "monster":
+                    current_health_hero = Battle.attack_monster(monster_1.attack, Hero.agility, current_health_hero, choice, knight_count)
+                    order = "hero"
+                    knight_count = knight_count + 1
+                else:
+                    print("Invalid Choice")
+            elif current_health_monster <= 0:
+                print("monster died")
+                break
+            elif current_health_hero <= 0:
+                print("you died")
+                break'''
 
 
-fight(Creature.skeleton, Creature.troll, Creature.giant_spider, Creature.orc)
+
+    fight(Creature.skeleton, Creature.troll, Creature.giant_spider, Creature.orc)
+
+def ai_game():
+    hero_selection = int(input("What class should AI be?\n 1: Knight \n 2: Mage \n 3: Thief \n"))
+    Ai.ai_class(hero_selection)
+    map_selection = int(input('Choose map size\n[1] Easy\n[2] Medium\n[3] Hard\n'))
+    Ai.ai_map(map_selection)
+
+selection = int(input("Who is playing? \n[1] Player \n[2] AI\n"))
+if selection == 1:
+    player_game()
+elif selection == 2:
+    ai_game()
 
--- Map.py
+++ Map.py
@@ -22,88 +22,91 @@ def mapsize(x, y):
     sizex = x
     sizey = y
 
-
-diffpick = input('Choose difficulty!\n[1] Easy\n[2] Medium\n[3] Hard\n')
-if diffpick == '1':
-    mapsize(4, 4)
-elif diffpick == '2':
-    mapsize(5, 5)
-elif diffpick == '3':
-    mapsize(8, 8)
+def map_choice():
+    diffpick = input('Choose difficulty!\n[1] Easy\n[2] Medium\n[3] Hard\n')
+    if diffpick == '1':
+        mapsize(4, 4)
+    elif diffpick == '2':
+        mapsize(5, 5)
+    elif diffpick == '3':
+        mapsize(8, 8)
 
 
 # Set corner coodinates
-ne = 0, sizey-1
-sw = sizex-1, 0
-se = sizex-1, sizey-1
+def set_coordninates():
+    ne = 0, sizey-1
+    sw = sizex-1, 0
+    se = sizex-1, sizey-1
 
 
 # Start room selection
-startroom = input('Were would you like to start?\n[1] Northwest\n[2] Northeast\n[3] Southwest\n[4] Southeast\n')
-if startroom == '1':
-    curx = 0
-    cury = 0
-elif startroom == '2':
-    curx = sizex - 1
-    cury = 0
-elif startroom == '3':
-    curx = 0
-    cury = sizey-1
-elif startroom == '4':
-    curx = sizex - 1
-    cury = sizey - 1
-
-print(cury, curx)
-
-# Choose direction
-while True:
-    direction = input("Choose direction\n[W] to go north\n[A] to go west\n[S] to go south\n[D] to go east\n").lower()
-    if direction == 'w':
-        prevx = curx
-        prevy = cury
-
-        for newroom in mapcord:
-            if newroom.gety() == (cury - 1) and newroom.getx() == curx:
-                cury = (newroom.gety())
-                break
-        if cury == prevy:
-            print("You search the wall for a door but are unable to find one.")
-            continue
-
-    elif direction == 's':
-        prevx = curx
-        prevy = cury
-
-        for newroom in mapcord:
-            if newroom.gety() == (cury + 1) and newroom.getx() == curx:
-                cury = (newroom.gety())
-                break
-        if cury == prevy:
-            print("You search the wall for a door but are unable to find one.")
-            continue
-
-    elif direction == 'a':
-        prevx = curx
-        prevy = cury
-
-        for newroom in mapcord:
-            if newroom.getx() == (curx - 1) and newroom.gety() == cury:
-                curx = (newroom.getx())
-                break
-        if curx == prevx:
-            print("You search the wall for a door but are unable to find one.")
-            continue
-
-    elif direction == 'd':
-        prevx = curx
-        prevy = cury
-
-        for newroom in mapcord:
-            if newroom.getx() == (curx + 1) and newroom.gety() == cury:
-                curx = (newroom.getx())
-                break
-        if curx == prevx:
-            print("You search the wall for a door but are unable to find one.")
-            continue
+def start_room():
+    startroom = input('Were would you like to start?\n[1] Northwest\n[2] Northeast\n[3] Southwest\n[4] Southeast\n')
+    if startroom == '1':
+        curx = 0
+        cury = 0
+    elif startroom == '2':
+        curx = sizex - 1
+        cury = 0
+    elif startroom == '3':
+        curx = 0
+        cury = sizey-1
+    elif startroom == '4':
+        curx = sizex - 1
+        cury = sizey - 1
 
     print(cury, curx)
+
+# Choose direction
+def choose_direction():
+    while True:
+        direction = input("Choose direction\n[W] to go north\n[A] to go west\n[S] to go south\n[D] to go east\n").lower()
+        if direction == 'w':
+            prevx = curx
+            prevy = cury
+
+            for newroom in mapcord:
+                if newroom.gety() == (cury - 1) and newroom.getx() == curx:
+                    cury = (newroom.gety())
+                    break
+            if cury == prevy:
+                print("You search the wall for a door but are unable to find one.")
+                continue
+
+        elif direction == 's':
+            prevx = curx
+            prevy = cury
+
+            for newroom in mapcord:
+                if newroom.gety() == (cury + 1) and newroom.getx() == curx:
+                    cury = (newroom.gety())
+                    break
+            if cury == prevy:
+                print("You search the wall for a door but are unable to find one.")
+                continue
+
+        elif direction == 'a':
+            prevx = curx
+            prevy = cury
+
+            for newroom in mapcord:
+                if newroom.getx() == (curx - 1) and newroom.gety() == cury:
+                    curx = (newroom.getx())
+                    break
+            if curx == prevx:
+                print("You search the wall for a door but are unable to find one.")
+                continue
+
+        elif direction == 'd':
+            prevx = curx
+            prevy = cury
+
+            for newroom in mapcord:
+                if newroom.getx() == (curx + 1) and newroom.gety() == cury:
+                    curx = (newroom.getx())
+                    break
+            if curx == prevx:
+                print("You search the wall for a door but are unable to find one.")
+                continue
+
+        print(cury, curx)
